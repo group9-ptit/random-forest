fa141c9cab1ca8ce13b2f5f4f6d2af482cc4dd91
UynUyn2k10103 <uyennt.b19cn709@stu.ptit.edu.vn>
2023-05-12 23:55:48 +0700

    add mutil-branch

--- src/type.py
+++
@@ -1,6 +0,0 @@
-from typing import Any, Dict, TypeVar, Union
-
-Record = TypeVar('Record', bound=Dict[str, float])
-Label = TypeVar('Label', bound=Union[str, int])
-CsvRow = TypeVar('CsvRow', bound=Dict[str, Any])
-CsvRowWithoutLabel = TypeVar('CsvRowWithoutLabel', bound=Dict[str, Any])
